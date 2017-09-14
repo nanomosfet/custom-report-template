af676c3b1040270ebda14e779a11d9cce5453ba7
nanomosfet <timmyzsearcy@gmail.com>
2017-09-14 16:34:17 -0700

    Reorginize structure of input list

--- custom-report-template/src/input-list.js
+++ custom-report-template/src/input-list.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import data from './data.js'
 
-class TextInput extends Component {
-    render() {
+class Input extends Component {
+    
+
+    renderTextInput(input) {
         return (
             <div className="input-group">
-                <span className="input-group-addon" id="age">Age</span>
-                <input type="text" className="form-control" placeholder="Enter Age" aria-label="Username" aria-describedby="age" />
+                <span className="input-group-addon" id={input.name}>{input.label}</span>
+                <input type={input.type} className="form-control" placeholder={"Enter " + input.label} aria-label={input.label} aria-describedby={input.name} />
             </div>
         );
+    }
+
+    renderRadioInput(input) {
+
+    }
+
+    render() {
+        var input = this.props.input;
+        return this.renderTextInput(input);
     }
 }
 
 class InputList extends Component {
     render() {
-        return <TextInput />
+        const input = data.sections[0].subSections[0].inputs[0];
+        return <Input input={input} />
     }
 
 }
